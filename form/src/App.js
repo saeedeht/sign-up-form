790525adbf36bc2830a781e1a573e18498012c3b
saeedeh torkamani <saeedehtorkamani@gmail.com>
2022-09-17 11:16:37 -0700

    added routes for login and signup pages

--- form/src/App.js
+++ form/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
-      <SignUp />
+      <Routes>
+        <Route path='/Login' elemet={<Login />} />
+        <Route path='/SignUp' elemet={<SignUp />} />
+      </Routes>
     </div>
   );
 };
